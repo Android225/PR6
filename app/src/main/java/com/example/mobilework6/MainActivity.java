9941887e96fd39e340a8af39b803490c0948f8d5
Andry <a.krishtofor@gmail.com>
2023-04-08 02:38:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mobilework6/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.mobilework6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int notificationId = 0;
+    private final String CHANNEL_ID = "channel_id";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Создание канала уведомлений
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel name", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("description");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+        // Баннер
+        if (ContextCompat.checkSelfPermission(this,
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 1);
+        }
+        Intent intent = new Intent(this, ServiceCustom.class);
+        startService(intent);
+        stopService(intent);
+
+    }
+    private void showNotification() {
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Hello world!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+        notificationManager.notify(notificationId, builder.build());
+        // notificationId - должен быть уникальным для каждого уведомления в канале
+    }
+    public void onClickBtn(View view){
+        Log.d("ShowNotification", "work method showNotification");
+        showNotification();
+    }
+}
